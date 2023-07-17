10b8af37df7e00eaba0b6781a08e25641292a046
Marek Miller <marek.miller@protonmail.com>
2023-07-17 22:13:56 +0200

    Update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # aramid
 
 Synthetic fibers
+
+- Fibers are little state machines that behave like coroutines: when `.run()`,
+  they yield first, and then they return. In the meantime, they carry their full
+  stack around with them.
+- Fibers are a model of concurrent computation. They are static, lightweight and
+  particularly well-suited for cooperative multitasking.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,62 @@
-pub fn add(
-    left: usize,
-    right: usize,
-) -> usize {
-    left + right
+//! Synthetic fibers.
+//!
+//! Fibers are little state machines that behave like coroutines: when run, they
+//! yield first, and then they return.  In the meantime, they carry their full
+//! stack around with them.
+//!
+//! Fibers are a model of concurrent computation.  They are static, lightweight
+//! and particularly well-suited for cooperative multitasking.
+
+pub enum FiberState<F>
+where
+    F: Fiber,
+{
+    Pending(F::Yield, F),
+    Done(F::Final),
+}
+
+pub trait Fiber
+where
+    Self: Sized,
+{
+    type Yield;
+    type Final;
+
+    fn run(self) -> FiberState<Self>;
+}
+
+/// Continuation
+pub enum Ctn<Y, R> {
+    Pending(Y, BoxedFiber<Y, R>),
+    Done(R),
+}
+
+pub struct BoxedFiber<Y, F> {
+    f: Box<dyn FnOnce() -> Ctn<Y, F>>,
+}
+
+impl<Y, F> BoxedFiber<Y, F> {
+    pub fn new<OP>(f: OP) -> Self
+    where
+        OP: FnOnce() -> Ctn<Y, F> + 'static,
+    {
+        let f = Box::new(f);
+        Self {
+            f,
+        }
+    }
+}
+
+impl<Y, F> Fiber for BoxedFiber<Y, F> {
+    type Final = F;
+    type Yield = Y;
+
+    fn run(self) -> FiberState<Self> {
+        match (self.f)() {
+            Ctn::Pending(y, fibr) => FiberState::Pending(y, fibr),
+            Ctn::Done(res) => FiberState::Done(res),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -10,8 +64,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn boxed_fiber_01() {
+        let fibr = BoxedFiber::new(|| {
+            println!("Hello from fiber");
+            Ctn::Pending(
+                (),
+                BoxedFiber::new(|| {
+                    println!("Hello from continuation");
+                    Ctn::Done(())
+                }),
+            )
+        });
+
+        let mut state = fibr.run();
+        println!("Interlude");
+        if let FiberState::Pending(_, fibr) = state {
+            state = fibr.run();
+        }
+        if let FiberState::Done(_) = state {
+            println!("Done.");
+        }
     }
 }
