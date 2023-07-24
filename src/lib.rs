7490968be94538a0c96428e39b5f1328af60fd37
Marek Miller <marek.miller@protonmail.com>
2023-07-25 00:09:18 +0200

    Redefine API

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,29 @@
 //! Synthetic fibers 🧵
+//!
+//! Lightweight coroutines for cooperative multitasking.
 
-/// Lightweight coroutines for cooperative multitasking.
-pub trait Fiber {
-    /// The type of the yielded values.
-    type Yield<'a>
+/// Coroutine.
+pub trait Coro<T, R = ()> {
+    fn coro<F>(
+        &self,
+        f: F,
+    ) -> R
     where
-        Self: 'a;
-    /// The type of the final output produced by the fiber.
-    type Return;
-
-    /// Run the fiber until it yields.
-    fn run(&mut self) -> State<Self::Yield<'_>, Self::Return>;
+        for<'a> F: FnOnce(&'a T) -> R;
 }
 
-/// State of the fiber
+/// Fiber.
+pub trait Fiber {
+    type Yield;
+    type Return;
+    type Coroutine<'a>: Coro<Self::Yield>
+    where
+        Self: 'a;
+
+    fn next(&mut self) -> State<Self::Coroutine<'_>, Self::Return>;
+}
+
+/// Fiber state.
 #[derive(Debug, PartialEq)]
 #[must_use]
 pub enum State<Y, R> {
@@ -38,10 +48,33 @@ impl<Y, R> State<Y, R> {
     ///
     /// assert_eq!(state.unwrap(), 9);
     /// ```
+    #[inline]
     pub fn unwrap(self) -> Y {
+        self.expect("should be Yield")
+    }
+
+    /// Unwrap the value wrapped in `Yield`.
+    ///
+    /// # Panics
+    ///
+    /// This function panics with a custom message, if the state is `Done`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use aramid::State;
+    /// let state = State::<u8, ()>::Yield(9);
+    ///
+    /// assert_eq!(state.expect("not done yet"), 9);
+    /// ```
+    #[inline]
+    pub fn expect(
+        self,
+        msg: &str,
+    ) -> Y {
         match self {
             State::Yield(yld) => yld,
-            State::Done(_) => panic!("state is Yield"),
+            State::Done(_) => panic!("{msg}"),
         }
     }
 
@@ -59,9 +92,32 @@ impl<Y, R> State<Y, R> {
     ///
     /// assert_eq!(state.unwrap_done(), 9);
     /// ```
+    #[inline]
     pub fn unwrap_done(self) -> R {
+        self.expect_done("should be Done")
+    }
+
+    /// Unwrap the value wrapped in `Done`.
+    ///
+    /// # Panics
+    ///
+    /// This function panics with a custom message, if the state is `Yield`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use aramid::State;
+    /// let state = State::<(), u8>::Done(9);
+    ///
+    /// assert_eq!(state.expect_done("should be done by now"), 9);
+    /// ```
+    #[inline]
+    pub fn expect_done(
+        self,
+        msg: &str,
+    ) -> R {
         match self {
-            State::Yield(_) => panic!("state is Done"),
+            State::Yield(_) => panic!("{msg}"),
             State::Done(out) => out,
         }
     }
@@ -103,7 +159,7 @@ impl<Y, R> State<Y, R> {
     /// Return the value of `Done`, or apply operator `OP` on the value of
     /// `Yield`.
     ///
-    /// Returns  `None` if the value was `Yield`.
+    /// Returns `None` if the value was `Yield`.
     pub fn done_or<OP>(
         &mut self,
         f: OP,
@@ -135,98 +191,5 @@ impl<Y, R> State<Y, R> {
         } else {
             None
         }
-    }
-}
-
-pub struct HeapJob<'a, T> {
-    f: Box<dyn FnMut() -> State<HeapJob<'a, T>, T> + 'a>,
-}
-
-impl<'a, T> HeapJob<'a, T> {
-    pub fn new<F>(f: F) -> Self
-    where
-        F: FnMut() -> State<Self, T> + 'a,
-    {
-        Self {
-            f: Box::new(f)
-        }
-    }
-}
-
-impl<'a, T> Fiber for HeapJob<'a, T> {
-    type Return = T;
-    type Yield<'any> = () where Self: 'any;
-
-    fn run(&mut self) -> State<Self::Yield<'_>, Self::Return> {
-        match (self.f)() {
-            State::Yield(yld) => {
-                self.f = yld.f;
-                State::Yield(())
-            }
-            State::Done(res) => State::Done(res),
-        }
-    }
-}
-
-pub fn continue_with<'a, F, T>(f: F) -> State<HeapJob<'a, T>, T>
-where
-    F: FnMut() -> State<HeapJob<'a, T>, T> + 'a,
-{
-    State::Yield(HeapJob::new(f))
-}
-
-#[cfg(test)]
-mod tests {
-    use State::Done;
-
-    use super::*;
-
-    #[test]
-    fn heapjob_01() {
-        let mut fbr = HeapJob::new(|| Done(55.5));
-        assert_eq!(fbr.run(), Done(55.5));
-    }
-
-    #[test]
-    fn heapjob_02() {
-        let mut fbr = HeapJob::new(|| {
-            let res = 55.5;
-            continue_with(move || Done(res))
-        });
-        assert_eq!(fbr.run(), State::Yield(()));
-        assert_eq!(fbr.run(), Done(55.5));
-    }
-
-    #[test]
-    fn heapjob_03() {
-        let mut fbr = HeapJob::new(|| {
-            let res = 11;
-            continue_with(move || {
-                let res = res * 5;
-                continue_with(move || Done(res))
-            })
-        });
-        assert_eq!(fbr.run(), State::Yield(()));
-        assert_eq!(fbr.run(), State::Yield(()));
-        assert_eq!(fbr.run(), Done(55));
-    }
-
-    #[test]
-    fn heapjob_04() {
-        let continuation = |res| {
-            continue_with(move || {
-                let res = res * 5;
-                continue_with(move || Done(res))
-            })
-        };
-
-        let mut fbr = HeapJob::new(move || {
-            let res = 11;
-            continuation(res)
-        });
-
-        assert_eq!(fbr.run(), State::Yield(()));
-        assert_eq!(fbr.run(), State::Yield(()));
-        assert_eq!(fbr.run(), Done(55));
     }
 }
